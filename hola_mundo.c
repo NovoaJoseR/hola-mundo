a011583f5ee194e77d3dc0e1f094086add576ef1
Jose Ramón Novoa <NovoaJoseR@users.noreply.github.com>
2015-07-31 13:23:17 +0200

    Create hola_mundo.c
    Escribo el programa en este fichero.

---
+++ hola_mundo.c
@@ -0,0 +1,11 @@
+/* Esto sí es un fichero en C */
+/* prueba de GitHub           */
+
+#include <stdio.h>
+
+int main (void)
+    {
+        printf("Hola Mundo\n");
+        return(0);
+    }
+/* Se acabó*/
